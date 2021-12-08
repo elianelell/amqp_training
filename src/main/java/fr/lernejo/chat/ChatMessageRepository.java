b4bb52f5612f33cd82a840f306cb836ff030e495
elianelell <95282591+elianelell@users.noreply.github.com>
2021-12-08 15:35:19 +0100

    Initial commit

---
+++ src/main/java/fr/lernejo/chat/ChatMessageRepository.java
@@ -0,0 +1,14 @@
+package fr.lernejo.chat;
+
+import java.util.List;
+
+public class ChatMessageRepository {
+    public void addChatMessage(String message)
+    {
+
+    }
+    /*public List<String> getLastTenMessages()
+    {
+        return List;
+    }*/
+}
